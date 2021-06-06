7dc7822eafc5bf37c99f7db3017730bb26bd878f
jobinnevin <30589983+jobinnevin@users.noreply.github.com>
2021-06-06 21:40:31 +0300

    Add files via upload

---
+++ Node.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Node() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>
+        This was an amazing bootcamp taken by Shaurya Sinha. We covered
+        everything from scratch including Javascript, React Js
+      </p>
+    </div>
+  );
+}
+
+export default Node;
